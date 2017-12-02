cb561e1a25f22d18e0400b772bfc44ad93e36745
radu-matei <matei.radu94@gmail.com>
2017-12-02 16:57:54 +0200

    Handle worker init request and response
    Signed-off-by: radu-matei <matei.radu94@gmail.com>

--- worker/worker.go
+++ worker/worker.go
@@ -5,14 +5,30 @@ import (
 	"github.com/radu-matei/azure-functions-golang-worker/rpc"
 )
 
-func handleStreamingMessage(message *rpc.StreamingMessage) {
+func handleStreamingMessage(message *rpc.StreamingMessage, client *Client, eventStream rpc.FunctionRpc_EventStreamClient) {
 	switch m := message.Content.(type) {
 
 	case *rpc.StreamingMessage_WorkerInitRequest:
-		handleWorkerInitRequest(m)
+		handleWorkerInitRequest(m, client, eventStream)
 	}
 }
 
-func handleWorkerInitRequest(message *rpc.StreamingMessage_WorkerInitRequest) {
+func handleWorkerInitRequest(message *rpc.StreamingMessage_WorkerInitRequest, client *Client, eventStream rpc.FunctionRpc_EventStreamClient) {
 	log.Debugf("received worker init request with host version %s ", message.WorkerInitRequest.HostVersion)
+
+	workerInitResponse := &rpc.StreamingMessage{
+		RequestId: client.Cfg.RequestID,
+		Content: &rpc.StreamingMessage_WorkerInitResponse{
+			WorkerInitResponse: &rpc.WorkerInitResponse{
+				Result: &rpc.StatusResult{
+					Status: rpc.StatusResult_Success,
+				},
+			},
+		},
+	}
+
+	if err := eventStream.Send(workerInitResponse); err != nil {
+		log.Fatalf("Failed to worker init response: %v", err)
+	}
+	log.Debugf("sent start worker init response")
 }
