5bd235f77abe14c25778d184456d94007de76eb6
radu-matei <matei.radu94@gmail.com>
2017-12-03 11:52:58 +0200

    Update sample with Http request from Azure Functions
    Signed-off-by: radu-matei <matei.radu94@gmail.com>

--- sample/HttpTriggerGo/main.go
+++ sample/HttpTriggerGo/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"fmt"
-
 	log "github.com/Sirupsen/logrus"
+	"github.com/radu-matei/azure-functions-golang-worker/rpc"
 )
 
-// Run is the entrypoint to our Go Azure Function
-func Run() {
-	fmt.Println("Hello, Azure Functions!")
+// Run is the entrypoint to our Go Azure Function - if you want to change it, see function.json
+func Run(request *rpc.RpcHttp) {
 	log.SetLevel(log.DebugLevel)
-	log.Debugf("logging in Azure Function using external Go dependency")
+	log.Debugf("received function invocation request: %v", request)
+
+}
+
+func main() {
+	// this will be built using -buildmode=pluign, so there is no need for main
+	// only here to silence VSCode errors - don't use, will be removed
 }
