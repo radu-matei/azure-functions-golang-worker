4c50a9e75a408edf6f651aead3f049a77d588a5c
radu-matei <matei.radu94@gmail.com>
2017-12-03 03:08:35 +0200

    Functioning external dependenciesgit add .
    Signed-off-by: radu-matei <matei.radu94@gmail.com>

--- sample/HttpTriggerGo/main.go
+++ sample/HttpTriggerGo/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	log "github.com/Sirupsen/logrus"
 )
 
 // Run is the entrypoint to our Go Azure Function
 func Run() {
 	fmt.Println("Hello, Azure Functions!")
+	log.SetLevel(log.DebugLevel)
+	log.Debugf("logging in Azure Function using external Go dependency")
 }
