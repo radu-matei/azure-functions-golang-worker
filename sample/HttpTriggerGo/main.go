f49a0164c59457da5b778cfdf94d5e72feeaa786
radu-matei <matei.radu94@gmail.com>
2017-12-05 01:41:16 +0200

    Update sample to use runtime.HTTPRequest
    Signed-off-by: radu-matei <matei.radu94@gmail.com>

--- sample/HttpTriggerGo/main.go
+++ sample/HttpTriggerGo/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 
 	log "github.com/Sirupsen/logrus"
-	"github.com/radu-matei/azure-functions-golang-worker/rpc"
+	"github.com/radu-matei/azure-functions-golang-worker/runtime"
 )
 
 // Run is the entrypoint to our Go Azure Function - if you want to change it, see function.json
-func Run(request *rpc.RpcHttp) []byte {
+func Run(request *runtime.HTTPRequest, ctx *runtime.Context) []byte {
 	log.SetLevel(log.DebugLevel)
 
 	u := User{
@@ -18,6 +19,9 @@ func Run(request *rpc.RpcHttp) []byte {
 	}
 
 	log.Debugf("user: %v", u)
+
+	hah, _ := ioutil.ReadAll(request.Body)
+	log.Debugf("request body: %v", string(hah))
 
 	b, err := json.Marshal(u)
 	if err != nil {
