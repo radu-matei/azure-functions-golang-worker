2d72f0a6b7608cb72e8b6e30e5e8fac38f1258e0
Radu Matei <matei.radu94@gmail.com>
2017-12-16 17:48:19 +0200

    Add bindings file in azfunc package

---
+++ azfunc/binding.go
@@ -0,0 +1,43 @@
+package azfunc
+
+import (
+	"reflect"
+
+	"github.com/radu-matei/azure-functions-golang-worker/rpc"
+)
+
+// TODO - add other binding and trigger types
+// TODO - in the end, every trigger is a binding - does it make sense to have separate types for trigger / binding?
+const (
+	// HTTPTriggerType represents a HTTP trigger in function load request from the host
+	HTTPTriggerType = "httpTrigger"
+
+	// BlobTriggerType represents a blob trigger in function load request from host
+	BlobTriggerType = "blobTrigger"
+
+	// HTTPBindingType represents a HTTP binding in function load request from the host
+	HTTPBindingType = "http"
+
+	// BlobBindingType represents a blob binding in function load request from the host
+	BlobBindingType = "blob"
+)
+
+// StringToType - Because we don't have go/types information, we need to map the type info from the AST (which is string) to the actual types - see loader.go:83
+// investiage automatically adding here all types from package azfunc
+var StringToType = map[string]reflect.Type{
+	"*azfunc.HTTPRequest": reflect.TypeOf((*HTTPRequest)(nil)),
+	"*azfunc.Context":     reflect.TypeOf((*Context)(nil)),
+}
+
+// BindingType contains the Azure Functions binding type
+type BindingType string
+
+// Func contains a function symbol with in and out param types
+type Func struct {
+	Func             reflect.Value
+	Bindings         map[string]*rpc.BindingInfo
+	In               []reflect.Type
+	NamedInArgs      map[string]reflect.Type
+	Out              []reflect.Type
+	NamedOutBindings map[string]reflect.Value
+}
