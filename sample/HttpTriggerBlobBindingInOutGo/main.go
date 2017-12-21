15f5886647888958834402b180ca773f1d959894
Radu Matei <matei.radu94@gmail.com>
2017-12-22 01:52:17 +0200

    Initial sample for output bindings

---
+++ sample/HttpTriggerBlobBindingInOutGo/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/radu-matei/azure-functions-golang-worker/azfunc"
+)
+
+// Run is the entrypoint to our Go Azure Function - if you want to change it, see function.json
+func Run(req *azfunc.HTTPRequest, inBlob *azfunc.BlobInput, outBlob *azfunc.BlobInput, ctx *azfunc.Context) BlobData {
+	log.SetLevel(log.DebugLevel)
+
+	log.Debugf("function id: %s, invocation id: %s", ctx.FunctionID, ctx.InvocationID)
+
+	d := BlobData{
+		Name: req.Query["name"],
+		Data: inBlob.Data,
+	}
+
+	outBlob.Data = "Leeeet's hope this doesn't miserably fail..."
+
+	return d
+}
+
+// BlobData mocks any struct (or pointer to struct) you might want to return
+type BlobData struct {
+	Name string
+	Data string
+}
